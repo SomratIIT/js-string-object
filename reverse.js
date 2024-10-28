4a1a4321682a29a5791f9ef8802242c14eecbd77
SomratIIT <101197949+SomratIIT@users.noreply.github.com>
2024-10-28 21:45:26 +0600

    reverse a string

---
+++ reverse.js
@@ -0,0 +1,4 @@
+let str = "Hello, World!";
+let reversedStr = str.split("").reverse().join("");
+
+console.log("Reversed String:", reversedStr);
---
+++ slice.js
@@ -0,0 +1,20 @@
+ const address = 'Aushnara';
+
+
+//  console.log(address.slice(0,4));
+
+const football =['Rahim', 'Karim','Jasim'];
+
+// console.log(football.split(' '));
+
+// console.log(football.join('-'));
+
+
+// Concat
+
+const first = 'R.H.';
+const second ='Somrat';
+
+// const fullname1 = first + ' ' + second ;
+const fullname2 = first.concat(' ').concat(second);
+console.log(fullname2);
